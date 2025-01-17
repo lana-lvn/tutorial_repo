b251a49a131231058b65c476bf4aa3c9502b4b8e
ruslana <ruslana.lohvyn@gmail.com>
2025-01-17 14:12:19 +0200

    'Init_m6l2'

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <header>
+      <h2>Redux toolkit + Slices</h2>
+    </header>
+  );
+};
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+import './index.css';
+import 'modern-normalize';
+import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+    <Toaster />
+  </Provider>
+);
